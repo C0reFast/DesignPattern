2684499b2507f8244efcb8f82c27e3c65b75d698
C0reFast <topchenfu@qq.com>
2013-05-20 15:06:30 +0800

    Simple Factory Pattern

---
+++ SimpleFactory/SimpleFactory/OperationFactory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleFactory
+{
+    public class OperationFactory
+    {
+        public static Operation CreateOperation(string op)
+        {
+            Operation oper = null;
+            switch (op)
+            {
+                case "+":
+                    oper = new OperationAdd();
+                    break;
+                case "-":
+                    oper = new OperationSub();
+                    break;
+                case "*":
+                    oper = new OperationMul();
+                    break;
+                case "/":
+                    oper = new OperationDiv();
+                    break;
+            }
+            return oper;
+        }
+    }
+}
---
+++ SimpleFactory/SimpleFactory/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SimpleFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation op = OperationFactory.CreateOperation("+");
+            op.NumberA = 10;
+            op.NumberB = 20;
+            Console.WriteLine(op.GetResult());
+        }
+    }
+}
