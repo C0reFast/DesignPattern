1d6b6a02f0a132311836cd36b4334039906845e4
C0reFast <topchenfu@qq.com>
2013-05-28 19:23:52 +0800

    Proxy Pattern

---
+++ ProxyPattern/ProxyPattern/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Proxy proxy = new Proxy();
+            proxy.Request();
+        }
+    }
+}
---
+++ ProxyPattern/ProxyPattern/Proxy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProxyPattern
+{
+    public class Proxy : ISubject
+    {
+        RealSubject realSubject;
+
+        public void Request()
+        {
+            if (realSubject == null)
+            {
+                realSubject = new RealSubject();
+            }
+            PreRequest();
+            realSubject.Request();
+            PostRequest();
+        }
+
+        public void PreRequest()
+        {
+            Console.WriteLine("Proxy.PreRequest()");
+        }
+
+        public void PostRequest()
+        {
+            Console.WriteLine("Proxy.PostRequest()");
+        }
+    }
+}
